e9dfafbde6d6476de4e8407bb57858c57030fd46
Félix BOROT <fborot@divalto.com>
2022-01-04 21:58:08 +0100

    refactor

--- Samples/WebDemo/ngclient/src/app/PowerFx/PowerFxCompletion.ts
+++ Samples/WebDemo/ngclient/src/app/PowerFx/PowerFxCompletion.ts
@@ -1,56 +1,6 @@
-import { CompletionItemKind, CompletionTriggerKind, DiagnosticSeverity } from "vscode-languageserver-protocol";
+import { CompletionItemKind, CompletionTriggerKind } from "vscode-languageserver-protocol";
 
-export const editorFontFamily = "'Menlo', 'Consolas', monospace,sans-serif";
-export const editorFontSize = 14;
-
-/** The default option configuration that we use for all the Monaco code editors inside PowerFx. */
-export const defaultEditorOptions: monaco.editor.IStandaloneEditorConstructionOptions = {
-//monaco.editor.IEditorConstructionOptions & monaco.editor.IGlobalEditorOptions = {
-  fontSize: editorFontSize,
-  lineDecorationsWidth: 4,
-  scrollbar: {
-    vertical: 'auto',
-    verticalScrollbarSize: 8,
-    horizontal: 'auto',
-    horizontalScrollbarSize: 8
-  },
-  // This fixes the first time render bug, and handles additional resizes.
-  automaticLayout: true,
-  contextmenu: false,
-  // Don't show a border above and below the current line in the editor.
-  renderLineHighlight: 'none',
-  lineNumbers: 'off',
-  wordWrap: 'on',
-  autoClosingBrackets: 'never',
-  quickSuggestions: true,
-  scrollBeyondLastLine: false,
-  // Don't show the minimap (the scaled down thumbnail view of the code)
-  minimap: { enabled: false },
-  selectionClipboard: false,
-  // Don't add a margin on the left to render special editor symbols
-  glyphMargin: false,
-  revealHorizontalRightPadding: 24,
-  find: {
-    seedSearchStringFromSelection: 'never',
-    autoFindInSelection: 'never'
-  },
-  suggestOnTriggerCharacters: true,
-  codeLens: false,
-  // Don't allow the user to collapse the curly brace sections
-  folding: false,
-  formatOnType: true,
-  fontFamily: editorFontFamily,
-  wordBasedSuggestions: false,
-  // This option helps to fix some of the overflow issues when using the suggestion widget in grid rows
-  // NOTE: This doesn't work when it's hosted inside Fluent Callout control
-  // More details in https://github.com/microsoft/monaco-editor/issues/2503
-  fixedOverflowWidgets: true,
-  language: 'powerfx'
-};
-
-
-
-export const _getCompletionTriggerKind = (
+export const getCompletionTriggerKind = (
   kind: monaco.languages.CompletionTriggerKind
 ): CompletionTriggerKind => {
   switch (kind) {
@@ -65,7 +15,7 @@ export const _getCompletionTriggerKind = (
   }
 };
 
-export const _getCompletionKind = (kind?: CompletionItemKind): monaco.languages.CompletionItemKind => {
+export const getCompletionKind = (kind?: CompletionItemKind): monaco.languages.CompletionItemKind => {
   switch (kind) {
     case CompletionItemKind.Text:
       return monaco.languages.CompletionItemKind.Text;
@@ -119,20 +69,5 @@ export const _getCompletionKind = (kind?: CompletionItemKind): monaco.languages.
       return monaco.languages.CompletionItemKind.TypeParameter;
     default:
       return monaco.languages.CompletionItemKind.Method;
-  }
-};
-
-export const _getMarkerSeverity = (severity?: DiagnosticSeverity): monaco.MarkerSeverity => {
-  switch (severity) {
-    case DiagnosticSeverity.Error:
-      return monaco.MarkerSeverity.Error;
-    case DiagnosticSeverity.Hint:
-      return monaco.MarkerSeverity.Hint;
-    case DiagnosticSeverity.Information:
-      return monaco.MarkerSeverity.Info;
-    case DiagnosticSeverity.Warning:
-      return monaco.MarkerSeverity.Warning;
-    default:
-      return monaco.MarkerSeverity.Error;
   }
 };
--- Samples/WebDemo/ngclient/src/app/editor/editor.component.ts
+++ Samples/WebDemo/ngclient/src/app/editor/editor.component.ts
@@ -2,41 +2,33 @@ import {
   EventEmitter,
   ChangeDetectorRef,
   Component,
-  ElementRef,
-  OnInit,
   Output,
-  ViewChild,
 } from '@angular/core';
 import {
-  NuMonacoEditorComponent,
   NuMonacoEditorEvent,
   NuMonacoEditorModel,
 } from '@ng-util/monaco-editor';
+import { Context } from 'vm';
 
 import {
   Diagnostic,
   PublishDiagnosticsParams,
 } from 'vscode-languageserver-protocol';
 import { HttpService } from '../http.service';
-import {
-  defaultEditorOptions,
-  _getCompletionKind,
-  _getCompletionTriggerKind,
-  _getMarkerSeverity,
-} from '../monaco/defaultEditor';
+import { getCompletionKind, getCompletionTriggerKind } from '../PowerFx/PowerFxCompletion';
+import { getMarkerSeverity } from '../PowerFx/PowerFxDiagnostic';
+import { defaultEditorOptions } from '../PowerFx/PowerFxEditor';
 import {
   PowerFxLanguageClient,
   PublishTokensParams,
   TokenResultType,
-} from '../monaco/PowerFxLanguageClient';
+} from '../PowerFx/PowerFxLanguageClient';
 import {
   addProvidersForModel,
   ensureLanguageRegistered,
-} from '../monaco/PowerFxSyntax';
-import { HighlightedName, NameKind } from '../monaco/PowerFxSyntaxTypes';
-import { ensureThemeSetup } from '../monaco/PowerFxTheme';
-
-// import * as monaco from 'monaco-editor'
+} from '../PowerFx/PowerFxSyntax';
+import { HighlightedName, NameKind } from '../PowerFx/PowerFxSyntaxTypes';
+import { ensureThemeSetup } from '../PowerFx/PowerFxTheme';
 
 @Component({
   selector: 'app-editor',
@@ -44,135 +36,127 @@ import { ensureThemeSetup } from '../monaco/PowerFxTheme';
   styleUrls: ['./editor.component.css'],
 })
 export class EditorComponent {
-  @ViewChild('editor')
-  public editor?: NuMonacoEditorComponent;
+  public editorOptions = defaultEditorOptions;
+  public model?: NuMonacoEditorModel;
+  private modelMonaco?: monaco.editor.ITextModel | null = null;
+  private languageClient?: PowerFxLanguageClient;
+  private normalizedCompletionLookup: { [lowercase: string]: string } = {};
+  private version: number = 0;
+  private onNamesChanged: (names: HighlightedName[]) => void = () => null;
 
-  editorOptions = defaultEditorOptions;
-  model?: NuMonacoEditorModel;
-  modelMonaco?: monaco.editor.ITextModel | null = null;
-  languageClient?: PowerFxLanguageClient;
-  _normalizedCompletionLookup: { [lowercase: string]: string } = {};
-  private _version: number = 0;
-  private _onNamesChanged: (names: HighlightedName[]) => void = () => null;
   @Output()
   public onChange = new EventEmitter<string>();
 
   constructor(private http: HttpService, private cdr: ChangeDetectorRef) {}
 
+  private getDocumentUriAsync = async () => 'powerfx://demo';
+
   showEvent(e: NuMonacoEditorEvent) {
     if (e.type === 'init') {
-      const modelUri = monaco.Uri.parse('powerfx://demo');
-
-      this.model = {
-        language: 'powerfx',
-        uri: modelUri,
-        value: 'CountRows(Sequence(9))',
-      };
-
-      this.cdr.detectChanges();
-
-      const model = monaco.editor.getModel(modelUri);
-      this.modelMonaco = model;
-      if (!this.modelMonaco) return;
-
-
-      ensureThemeSetup(monaco);
-      ensureLanguageRegistered(monaco, {
-        useSemicolons: false,
-        highlightedNames: [],
-        subscribeToNames: (namesChanged) => {
-          this._onNamesChanged = namesChanged;
-        },
-        getNormalizedCompletionLookup: () => this._normalizedCompletionLookup,
-      });
-
-      this.languageClient = new PowerFxLanguageClient(
-        async () => 'powerfx://demo',
-        async (payload: string) => {
-          //TODO send server
-          await this.sendAsync(payload);
-        },
-        (params: PublishDiagnosticsParams): void => {
-          if (!monaco || !this.modelMonaco) {
-            return;
-          }
-
-          const markers = params.diagnostics.map((error: Diagnostic) => {
-            const { start, end } = error.range;
-            return {
-              severity: _getMarkerSeverity(error.severity),
-              message: error.message,
-              startLineNumber: start.line,
-              startColumn: start.character,
-              endLineNumber: end.line,
-              endColumn: end.character + 1, // end character is included
-            };
-          });
-
-          monaco.editor.setModelMarkers(this.modelMonaco, '', markers);
-        },
-        (params: PublishTokensParams): void => {
-
-          if (!monaco || !this.modelMonaco) {
-            return;
-          }
-
-          const names: HighlightedName[] = [];
-          for (const [key, value] of Object.entries(params.tokens)) {
-            this._normalizedCompletionLookup[key.toLowerCase()] = key;
-            switch (value) {
-              case TokenResultType.Function:
-                names.push({ name: key, kind: NameKind.Function });
-                break;
-
-              case TokenResultType.Variable:
-                names.push({ name: key, kind: NameKind.Variable });
-                break;
-
-              case TokenResultType.HostSymbol:
-                names.push({ name: key, kind: NameKind.HostSymbol });
-                break;
-
-              default:
-                break;
-            }
-          }
-
-          this._onNamesChanged?.(names);
-        }
-      );
-
-      addProvidersForModel(
-        this.modelMonaco,
-        this.provideCompletionItems,
-        this.provideSignatureHelp
-      );
-
-      this.languageClient.notifyDidOpenAsync(this.modelMonaco.getValue());
-
-      const monacoEditor = e.editor as monaco.editor.IStandaloneCodeEditor;
-      if (!monacoEditor) return;
-
-      this.modelMonaco.onDidChangeContent((e) => {
-       const value = this.modelMonaco?.getValue();
-        this.onChange.emit(value);
-        this.languageClient?.notifyDidChangeAsync(value || "", this._version++);
-      });
-
-      // this._disposeEditorSubscriptions();
-
-      // this._subscriptions.push(monacoEditor.onDidBlurEditorWidget(this._onBlur));
-      // this._subscriptions.push(monacoEditor.onDidFocusEditorWidget(this._onFocus));
-      // this._subscriptions.push(monacoEditor.onDidChangeCursorPosition(this._onDidChangeCursorPosition));
-      // this._subscriptions.push(monacoEditor.onKeyDown(this._onKeyDown));
-      // this._subscriptions.push(monacoEditor.onKeyUp(this._onKeyUp));
-      // this._subscriptions.push(this.modelMonaco.onDidChangeContent(this._onChange));
+      this.onMonacoInit();
     }
   }
 
-  private async sendAsync(data: string) {
-    // console.log('[LSP Client] Send: ' + data);
+  private onMonacoInit() {
+    const modelUri = monaco.Uri.parse('powerfx://demo');
 
+    this.model = {
+      language: 'powerfx',
+      uri: modelUri,
+    };
+
+    this.cdr.detectChanges();
+
+    const model = monaco.editor.getModel(modelUri);
+    this.modelMonaco = model;
+    if (!this.modelMonaco) return;
+
+    ensureThemeSetup(monaco);
+    ensureLanguageRegistered(monaco, {
+      useSemicolons: false,
+      highlightedNames: [],
+      subscribeToNames: (namesChanged) => {
+        this.onNamesChanged = namesChanged;
+      },
+      getNormalizedCompletionLookup: () => this.normalizedCompletionLookup,
+    });
+
+    this.languageClient = new PowerFxLanguageClient(
+      this.getDocumentUriAsync,
+      this.sendToLanguageServerAsync,
+      this.handleDiagnosticsNotification,
+      this.handleTokensNotification
+    );
+
+    addProvidersForModel(
+      this.modelMonaco,
+      this.provideCompletionItemsAsync,
+      this.provideSignatureHelpAsync
+    );
+
+    this.languageClient.notifyDidOpenAsync(this.modelMonaco.getValue());
+
+    this.modelMonaco.onDidChangeContent((e) => {
+      const value = this.modelMonaco?.getValue();
+      this.onChange.emit(value);
+      this.languageClient?.notifyDidChangeAsync(value || '', this.version++);
+    });
+  }
+
+  private async sendToLanguageServerAsync(payload: string) {
+    await this.sendAsync(payload);
+  }
+
+  private handleTokensNotification(params: PublishTokensParams) {
+    if (!monaco || !this.modelMonaco) {
+      return;
+    }
+
+    const names: HighlightedName[] = [];
+    for (const [key, value] of Object.entries(params.tokens)) {
+      this.normalizedCompletionLookup[key.toLowerCase()] = key;
+      switch (value) {
+        case TokenResultType.Function:
+          names.push({ name: key, kind: NameKind.Function });
+          break;
+
+        case TokenResultType.Variable:
+          names.push({ name: key, kind: NameKind.Variable });
+          break;
+
+        case TokenResultType.HostSymbol:
+          names.push({ name: key, kind: NameKind.HostSymbol });
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    this.onNamesChanged?.(names);
+  }
+
+  private handleDiagnosticsNotification(params: PublishDiagnosticsParams) {
+    if (!monaco || !this.modelMonaco) {
+      return;
+    }
+
+    const markers = params.diagnostics.map((error: Diagnostic) => {
+      const { start, end } = error.range;
+      return {
+        severity: getMarkerSeverity(error.severity),
+        message: error.message,
+        startLineNumber: start.line,
+        startColumn: start.character,
+        endLineNumber: end.line,
+        endColumn: end.character + 1, // end character is included
+      };
+    });
+
+    monaco.editor.setModelMarkers(this.modelMonaco, '', markers);
+  }
+
+  private async sendAsync(data: string) {
     try {
       const result = await this.http.sendDataAsync('lsp', data);
       if (!result.ok) {
@@ -183,7 +167,6 @@ export class EditorComponent {
       if (response) {
         const responseArray = JSON.parse(response);
         responseArray.forEach((item: string) => {
-          // console.log('[LSP Client] Receive: ' + item);
           this.languageClient?.onDataReceivedFromLanguageServer(item);
         });
       }
@@ -192,39 +175,22 @@ export class EditorComponent {
     }
   }
 
-  private provideCompletionItems = (
+  private async provideCompletionItemsAsync(
     model: monaco.editor.ITextModel,
     position: monaco.Position,
     context: monaco.languages.CompletionContext
-  ) =>
-    // Note: we call model methods up-front to avoid disposed model issue
-    // since there are other async operations inside _provideCompletionAsync
-    this._provideCompletionAsync(
-      model.getValue(),
-      position,
-      model.getWordAtPosition(position),
-      context.triggerKind,
-      context.triggerCharacter
-    );
-
-  private _provideCompletionAsync = async (
-    currentText: string,
-    position: monaco.Position,
-    currentWordPosition: monaco.editor.IWordAtPosition | null,
-    triggerKind: monaco.languages.CompletionTriggerKind,
-    triggerCharacter?: string
-  ): Promise<monaco.languages.CompletionList> => {
-    // const { lspConfig } = this.props;
-    // if (lspConfig?.disableCompletionRequest) {
-    //   return { suggestions: [] };
-    // }
+  ) {
+    const currentText = model.getValue();
+    const currentWordPosition = model.getWordAtPosition(position);
+    const triggerKind = context.triggerKind;
+    const triggerCharacter = context.triggerCharacter;
 
     const result =
       await this.languageClient?.requestProvideCompletionItemsAsync(
         currentText,
         position.lineNumber - 1,
         position.column - 1,
-        _getCompletionTriggerKind(triggerKind),
+        getCompletionTriggerKind(triggerKind),
         triggerCharacter
       );
 
@@ -249,12 +215,12 @@ export class EditorComponent {
     const suggestions: monaco.languages.CompletionItem[] = result.items.map(
       (item) => {
         const label = item.label;
-        this._normalizedCompletionLookup[label.toLowerCase()] = label;
+        this.normalizedCompletionLookup[label.toLowerCase()] = label;
         return {
           label,
           documentation: item.documentation,
           detail: item.detail,
-          kind: _getCompletionKind(item.kind),
+          kind: getCompletionKind(item.kind),
           range,
           insertText: item.label,
         };
@@ -265,20 +231,16 @@ export class EditorComponent {
       incomplete: !currentWordPosition,
       suggestions,
     };
-  };
+  }
 
-  private provideSignatureHelp = (
+  private async provideSignatureHelpAsync(
     model: monaco.editor.ITextModel,
     position: monaco.Position,
     token: monaco.CancellationToken,
     context: monaco.languages.SignatureHelpContext
-  ) => this._provideSignatureHelpAsync(model.getValue(), position, context);
+  ): Promise<monaco.languages.SignatureHelpResult> {
+    const currentText = model.getValue();
 
-  private _provideSignatureHelpAsync = async (
-    currentText: string,
-    position: monaco.Position,
-    context: monaco.languages.SignatureHelpContext
-  ): Promise<monaco.languages.SignatureHelpResult> => {
     const noResult: monaco.languages.SignatureHelpResult = {
       value: {
         signatures: [],
@@ -289,11 +251,6 @@ export class EditorComponent {
         return;
       },
     };
-
-    // const { lspConfig } = this.props;
-    // if (!lspConfig?.enableSignatureHelpRequest) {
-    //   return noResult;
-    // }
 
     const result = await this.languageClient?.requestProvideSignatureHelpAsync(
       currentText,
@@ -320,5 +277,5 @@ export class EditorComponent {
         return;
       },
     };
-  };
+  }
 }
